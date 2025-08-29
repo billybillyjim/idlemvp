2aeb61642d77a64c8d320790b3acf05cabd7c9a9
billybillyjim <thereallmwels@gmail.com>
2025-08-29 07:17:16 -0500

    begin work on production modifiers

--- data/productionModifiers.js
+++ data/productionModifiers.js
@@ -18,5 +18,35 @@ export default [
         Requirements:{
             Technologies:["Bronze Tools"]
         }
+    },
+    {   
+        Name:"Well Poles", 
+        Boosts:[
+            {Name:"Farmer", Currency:"Food", ModifierType:"Additive", Amount:0.1,},
+        ],
+        Requirements:{
+            Technologies:["Well Poles"]
+        }
+    },
+    {   
+        Name:"Crop Rotations", 
+        Boosts:[
+            {Name:"Farmer", Currency:"Food", ModifierType:"Additive", Amount:0.5,},
+        ],
+        Requirements:{
+            Technologies:["Crop Rotations"]
+        }
+    },
+    {   
+        Name:"Rope", 
+        Boosts:[
+            {Name:"Farmer", Currency:"Rope", ModifierType:"Additive", Amount:0.1,},
+            {Name:"Lumberjack", Currency:"Rope", ModifierType:"Additive", Amount:0.1,},
+            {Name:"Unemployed", Currency:"Rope", ModifierType:"Additive", Amount:0.2,},
+            {Name:"Surveyor", Currency:"Rope", ModifierType:"Additive", Amount:0.2,},
+        ],
+        Requirements:{
+            Technologies:["Rope"]
+        }
     }
 ];
